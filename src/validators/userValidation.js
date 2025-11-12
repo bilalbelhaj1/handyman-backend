0569a10b88d76ed6ff38d73df481c0b678ccb197
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2025-11-12 14:52:53 +0100

    Update user register/login validation schema

--- src/validators/userValidation.js
+++ src/validators/userValidation.js
@@ -3,21 +3,26 @@ const Joi = require('joi')
 
 const userRegisterValidation = Joi.object({
     Name: Joi.string()
+       .label("First Name")
        .min(3)
        .max(30)
        .required(),
     lastName: Joi.string()
+       .label("Last Name")
        .min(3)
        .max(30)
        .required(),
     phoneNumber: Joi.string()
+       .label("Phone Number")
        .min(10)
        .max(10)
        .required(),
     province : Joi.string()
+       .label("Province")
        .max(100)
        .required(),
     city: Joi.string()
+       .label("City")
        .min(3)
        .max(50)
        .required()
@@ -25,10 +30,12 @@ const userRegisterValidation = Joi.object({
 
 const loginSchema = Joi.object({
     phone: Joi.string()
+       .label("Phone Number")
        .min(10)
        .max(10)
        .required(),
     password: Joi.string()
+       .label("Password")
        .min(8)
        .max(30)
        .required()
